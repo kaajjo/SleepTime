367c097225abde9e597b2ff43f3078de11b5b2cc
ItsKajjo <claymanoff@gmail.com>
2022-06-02 19:32:00 +0300

    add tabbar and viewpager2 to MainActivity.kt

--- app/src/main/java/com/kodama/sleeptime/ui/MainActivity.kt
+++ app/src/main/java/com/kodama/sleeptime/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kodama.sleeptime
+package com.kodama.sleeptime.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodama.sleeptime.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
